661f2103dfd0802ac71e13a94a048eca0180935d
Andrew Lin <andrew.lin@ieee.org>
2015-12-02 20:42:19 -0800

    Removed dead code

--- photoboothapp/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright 2015, andrew
-All rights reserved.
-
-This software is licensed under the BSD 3-Clause License.
-See LICENSE.txt at the root of the project or
-https://opensource.org/licenses/BSD-3-Clause
-"""
